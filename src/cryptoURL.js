a0a8762b68ba5e1bb89344d4bc57e2b02a5bb647
Rafael Caricio <rafael.jacinto@gmail.com>
2012-04-22 02:08:21 -0300

    MD5 crypt working.

--- src/cryptoURL.js
+++ src/cryptoURL.js
@@ -1,26 +1,70 @@
+var md5 = require('../lib/md5'),
+    aes = require('../lib/aes');
 
-
-var CryptoURL = function(securityKey, imageURL) {
+var CryptoURL = module.exports = function(securityKey, imageURL) {
     this.key = this.inflateKey(securityKey);
     this.imageURL = imageURL;
     this.filters = {};
 }
 
 CryptoURL.prototype =  {
+
     inflateKey: function(securityKey) {
-        while (securityKey.length() < 16) {
+        while (securityKey.length < 16) {
             securityKey += securityKey;
         }
         return securityKey.substring(0, 16);
     },
+
+    generate: function() {
+        var url = this.requestPath();
+        url = this.rightPad(url, '{');
+        return url + '/' + this.imageURL;
+    },
+
+    requestPath: function() {
+        if (!this.imageURL) {
+            throw 'The image url can\'t be null or empty.';
+        }
+        var parts = [];
+
+        if (this.width !== undefined || this.height !== undefined) {
+            parts.push((this.width === undefined ? '0' : this.width) + 'x' + (this.height === undefined ? '0' : this.height));
+        }
+
+        url = parts.join('/');
+        url += this.md5(url).data;
+        return url;
+    },
+
+    rightPad: function(url, padChar) {
+        var numberOfChars = 16 - url.length % 16;
+
+        if (!numberOfChars) {
+            return url;
+        }
+
+        for (var i=0; i < numberOfChars; i++) {
+            url += padChar;
+        }
+
+        return url;
+    },
+
+    md5: function(imageURL) {
+        var md = md5.create();
+        md.update(imageURL);
+        return md.digest();
+    },
+
     resize: function(width, height) {
+        this.width = width;
+        this.height = height;
+        return this;
+    },
+
+    toString: function() {
+        return this.generate();
     }
+
 };
-
-
-
-if(typeof(module) !== 'undefined' && module.exports) {
-    module.exports = {
-        CryptoURL: CryptoURL
-    };
-}
